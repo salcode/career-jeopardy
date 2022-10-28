3f1bbb6a018a5ce3e4823e537e8ee9165884d431
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-10-28 12:57:45 -0400

    Add "cell" className

--- src/components/Category.js
+++ src/components/Category.js
@@ -2,10 +2,10 @@ export default function Category({
   category,
 }) {
   return <div className="category">
-    <h2>{category.title}</h2>
+    <h2 className="cell">{category.title}</h2>
     {
       category.questions?.map(
-        (question) => <section key={question.text}>
+        (question) => <section className="cell" key={question.text}>
           {question.text}
         </section>
       )
