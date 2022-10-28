d182c00ef82b44fe4a4c88ff2894f1261d0a531f
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-10-28 16:57:13 -0400

    Add stub reducer to App

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
+import { useReducer } from 'react';
 import './App.css';
 import Board from './components/Board';
 import boardData from './data/boardData.js';
 
 function App() {
+  const [ state, dispatch ] = useReducer(
+    () => {},
+    {
+      boardData,
+      question: '',
+    },
+  );
   return (
     <div className="App">
       <header className="App-header">
@@ -10,7 +18,10 @@ function App() {
           Mr. Ferrarello
         </p>
       </header>
-      <Board boardData={boardData} />
+      <Board
+        boardData={state.boardData}
+        question={state.question}
+      />
     </div>
   );
 }
