02703bda6e2dedcd2ee18194610cc089e819dfa6
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-10-28 16:57:22 -0400

    Conditionally render question or board

--- src/components/Board.js
+++ src/components/Board.js
@@ -2,7 +2,12 @@ import Category from './Category';
 
 export default function Board({
   boardData,
+  question,
 }) {
+  if (question) {
+    return <h1>{question}</h1>;
+  }
+  // There is no question, display the board.
   return <div className="board-wrapper">
     {
       boardData.categories.map(
