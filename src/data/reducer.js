941ccda8472c2a928b159859b8d859ad6a6531e9
Sal Ferrarello <5194588+salcode@users.noreply.github.com>
2022-11-08 05:34:06 -0500

    Add action markRead to reducer

--- src/data/reducer.js
+++ src/data/reducer.js
@@ -1,5 +1,31 @@
 export default function reducer(state, action) {
   switch (action.type) {
+    case 'markRead': {
+      return {
+        ...state,
+        boardData: {
+          ...state.boardData,
+          categories: (state?.boardData?.categories ?? []).map(
+            (category) => {
+              return {
+                ...category,
+                questions: (category?.questions ?? []).map(
+                  (question) => {
+                    if (action.value === question.text) {
+                      return {
+                        ...question,
+                        isRead: true,
+                      }
+                    }
+                    return question;
+                  }
+                )
+              };
+            }
+          ),
+        },
+      };
+    }
     case 'setQuestion': {
       return {
         ...state,
